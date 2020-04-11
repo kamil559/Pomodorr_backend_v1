14e3dea565ed873f3f9fa0260d3417f3bf887969
Kamil <kamil.daniewski@gmail.com>
2020-04-11 10:02:01 +0200

    Fixed queries tests for users module.

--- pomodorr/users/tests/test_queries.py
+++ pomodorr/users/tests/test_queries.py
@@ -1,30 +1,28 @@
 import pytest
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
 pytestmark = pytest.mark.django_db
 
 
-def test_get_queryset(django_assert_num_queries):
+def test_get_queryset(user_model, django_assert_num_queries):
     with django_assert_num_queries(1):
-        repr(User.objects.all())
+        repr(user_model.objects.all())
 
 
-def test_get_active_standard_users(django_assert_num_queries):
+def test_get_active_standard_users(user_model, django_assert_num_queries):
     with django_assert_num_queries(1):
-        repr(User.objects.active_standard_users())
+        repr(user_model.objects.active_standard_users())
 
 
-def test_get_non_active_standard_users(django_assert_num_queries):
+def test_get_non_active_standard_users(user_model, django_assert_num_queries):
     with django_assert_num_queries(1):
-        repr(User.objects.non_active_standard_users())
+        repr(user_model.objects.non_active_standard_users())
 
 
-def test_get_blocked_standard_users(django_assert_num_queries):
+def test_get_blocked_standard_users(user_model, django_assert_num_queries):
     with django_assert_num_queries(1):
-        repr(User.objects.blocked_standard_users())
+        repr(user_model.objects.blocked_standard_users())
 
 
-def test_get_ready_to_unblock_users(django_assert_num_queries):
+def test_get_ready_to_unblock_users(user_model, django_assert_num_queries):
     with django_assert_num_queries(1):
-        repr(User.objects.ready_to_unblock_users())
+        repr(user_model.objects.ready_to_unblock_users())
