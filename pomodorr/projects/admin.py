e36795233110345978c1d3d93a1dfc93b007b8b8
Kamil <kamil.daniewski@gmail.com>
2020-04-14 20:25:28 +0200

    Changed custom admin page for Project model and adjusted the Project manager's queryset for a better usability purpose.

--- pomodorr/projects/admin.py
+++ pomodorr/projects/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from django.db import transaction
 
 from pomodorr.projects.models import Project
 
-admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'priority', 'user_defined_ordering', 'is_removed')
+    list_filter = ('is_removed',)
+    actions = ['hard_delete', 'undo_delete']
+    search_fields = ('user__username', 'name', 'color')
+
+    def get_queryset(self, request):
+        return self.model.all_objects.all()
+
+    def undo_delete(modeladmin, request, queryset):
+        with transaction.atomic():
+            queryset.update(is_removed=False)
+
+    def hard_delete(modeladmin, request, queryset):
+        with transaction.atomic():
+            queryset.delete(soft=False)
+
+    undo_delete.short_description = 'Undo deletion of selected projects'
+    hard_delete.short_description = 'Delete objects entirely from database'
--- pomodorr/projects/models.py
+++ pomodorr/projects/models.py
@@ -7,6 +7,22 @@ from django.utils.translation import gettext_lazy as _
 from model_utils.models import SoftDeletableModel
 
 
+class CustomProfileQueryset(models.QuerySet):
+    def delete(self, soft=True):
+        if soft:
+            self.update(is_removed=True)
+        else:
+            super(CustomProfileQueryset, self).delete()
+
+
+class CustomProfileManagerMixin:
+    _queryset_class = CustomProfileQueryset
+
+
+class CustomProfileManager(CustomProfileManagerMixin, models.Manager):
+    pass
+
+
 class Project(SoftDeletableModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(null=False, blank=False, max_length=128)
@@ -16,6 +32,8 @@ class Project(SoftDeletableModel):
     user = models.ForeignKey(to='users.User', null=False, blank=False, on_delete=models.CASCADE,
                              related_name='projects')
     created_at = models.DateTimeField(_('created at'), default=timezone.now, editable=False)
+
+    all_objects = CustomProfileManager()
 
     class Meta:
         unique_together = ['name', 'user']
