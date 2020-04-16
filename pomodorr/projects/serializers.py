96428a7ce609539d19be371576e49794d5c5ce46
Kamil <kamil.daniewski@gmail.com>
2020-04-16 19:26:48 +0200

    Adjusted serializers.py to the new changes from models and fixed related tests.

--- pomodorr/projects/serializers.py
+++ pomodorr/projects/serializers.py
@@ -1,16 +1,33 @@
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from pomodorr.projects.models import Project
+from pomodorr.projects.services import PriorityDomainModel, ProjectDomainModel
 from pomodorr.users.services import UserDomainModel
 
 
 class ProjectSerializer(ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(write_only=True, default=serializers.CurrentUserDefault(),
                                               queryset=UserDomainModel.get_active_standard_users())
-    priority = serializers.IntegerField(min_value=1)
+    priority = serializers.PrimaryKeyRelatedField(queryset=PriorityDomainModel.get_all_priorities())
     user_defined_ordering = serializers.IntegerField(min_value=1)
+
+    def validate_priority(self, value):
+        user = self.context['request'].user
+        if not PriorityDomainModel.get_priorities_for_user(user=user).filter(id=value.id).exists():
+            raise serializers.ValidationError(
+                _('Invalid pk "{pk_value}" - object does not exist.').format(pk_value=value.id), code='does_not_exist')
+
+    def validate(self, data):
+        #  Temporary solution for https://github.com/encode/django-rest-framework/issues/7100
+        user = self.context['request'].user
+        name = data.get('name') or None
+
+        if name is not None and ProjectDomainModel.get_active_projects_for_user(user=user, name=name).exists():
+            raise serializers.ValidationError(_('The fields name, user must make a unique set.'))
+        return data
 
     class Meta:
         model = Project
-        fields = ('id', 'name', 'priority', 'color', 'user_defined_ordering', 'user')
+        fields = ('id', 'name', 'priority', 'user_defined_ordering', 'user')
--- pomodorr/projects/tests/test_serializers.py
+++ pomodorr/projects/tests/test_serializers.py
@@ -1,6 +1,7 @@
 import random
 import factory
 import pytest
+from pytest_lazyfixture import lazy_fixture
 from rest_framework.exceptions import ValidationError
 
 from pomodorr.projects.serializers import ProjectSerializer
@@ -23,7 +24,8 @@ def test_serializer_single_project(project_instance):
     assert 'user' not in dict(serializer.data).keys()
 
 
-def test_serializer_save_with_valid_data(project_data, request_mock, active_user):
+def test_serializer_save_with_valid_data(project_data, request_mock, active_user, priority_instance):
+    project_data['priority'] = priority_instance.id
     request_mock.user = active_user
     serializer = ProjectSerializer(data=project_data)
     serializer.context['request'] = request_mock
@@ -37,9 +39,8 @@ def test_serializer_save_with_valid_data(project_data, request_mock, active_user
     [
         ('name', factory.Faker('pystr', max_chars=129).generate(), ValidationError),
         ('name', '', ValidationError),
-        ('color', factory.Faker('pystr', max_chars=19).generate(), ValidationError),
-        ('color', '', ValidationError),
         ('priority', random.randint(-999, -1), ValidationError),
+        ('priority', lazy_fixture('random_priority_id'), ValidationError),
         ('priority', '', ValidationError),
         ('user_defined_ordering', random.randint(-999, -1), ValidationError),
         ('user_defined_ordering', '', ValidationError)
@@ -56,8 +57,10 @@ def test_serializer_save_with_invalid_data(invalid_field_key, invalid_field_valu
     assert invalid_field_key in serializer.errors
 
 
-def test_serializer_save_with_unique_constraint_violated(project_data, project_instance, request_mock, active_user):
+def test_serializer_save_with_unique_constraint_violated(project_data, project_instance, request_mock,
+                                                         priority_instance, active_user):
     project_data['name'] = project_instance.name
+    project_data['priority'] = priority_instance.id
     request_mock.user = active_user
     serializer = ProjectSerializer(data=project_data)
     serializer.context['request'] = request_mock
@@ -73,7 +76,9 @@ def test_serializer_save_without_context_user(project_data):
         serializer.is_valid()
 
 
-def test_serializer_update_with_valid_data(project_data, project_instance, request_mock, active_user):
+def test_serializer_update_with_valid_data(project_data, project_instance, request_mock, priority_instance,
+                                           active_user):
+    project_data['priority'] = priority_instance.id
     request_mock.user = active_user
     serializer = ProjectSerializer(instance=project_instance, data=project_data)
     serializer.context['request'] = request_mock
@@ -87,9 +92,8 @@ def test_serializer_update_with_valid_data(project_data, project_instance, reque
     [
         ('name', factory.Faker('pystr', max_chars=129).generate(), ValidationError),
         ('name', '', ValidationError),
-        ('color', factory.Faker('pystr', max_chars=19).generate(), ValidationError),
-        ('color', '', ValidationError),
         ('priority', random.randint(-999, -1), ValidationError),
+        ('priority', lazy_fixture('random_priority_id'), ValidationError),
         ('priority', '', ValidationError),
         ('user_defined_ordering', random.randint(-999, -1), ValidationError),
         ('user_defined_ordering', '', ValidationError)
@@ -107,8 +111,9 @@ def test_serializer_update_with_invalid_data(invalid_field_key, invalid_field_va
 
 
 def test_serializer_update_with_unique_constraint_violated(project_data, project_instance, project_create_batch,
-                                                           request_mock, active_user):
+                                                           request_mock, priority_instance, active_user):
     project_data['name'] = project_create_batch[0].name
+    project_data['priority'] = priority_instance.id
     request_mock.user = active_user
     serializer = ProjectSerializer(instance=project_instance, data=project_data)
     serializer.context['request'] = request_mock
