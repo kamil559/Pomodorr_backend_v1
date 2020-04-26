6971c08cd45e38b7a4fd3cae6f0f8b5db013b25a
Kamil <kamil.daniewski@gmail.com>
2020-04-26 18:17:18 +0200

    Added new permission for Task model alongside with related tests.

--- pomodorr/tools/tests/test_permissions.py
+++ pomodorr/tools/tests/test_permissions.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 import pytest
 from django.contrib.auth.models import AnonymousUser
 
-from pomodorr.tools.permissions import IsObjectOwner, IsNotAuthenticated
+from pomodorr.tools.permissions import IsObjectOwner, IsNotAuthenticated, IsTaskOwner
 
 pytestmark = pytest.mark.django_db
 
@@ -72,6 +72,43 @@ def test_is_not_authenticated_with_authenticated_user():
     check_permission = is_not_authenticated_permission.has_permission(
         request=mock_request,
         view=mock_view
+    )
+
+    assert check_permission is False
+
+
+def test_is_task_project_owner_successfully(active_user):
+    mock_request = Mock()
+    mock_obj = Mock()
+    mock_view = Mock()
+
+    mock_request.user = active_user
+    mock_obj.project.user = active_user
+
+    is_object_owner_permission = IsTaskOwner()
+    check_permission = is_object_owner_permission.has_object_permission(
+        request=mock_request,
+        view=mock_view,
+        obj=mock_obj
+    )
+
+    assert check_permission is True
+
+
+def test_is_task_project_owner_with_different_user(active_user):
+    mock_request = Mock()
+    mock_obj = Mock()
+    mock_view = Mock()
+    obj_owner = Mock()
+
+    mock_request.user = active_user
+    mock_obj.project.user = obj_owner
+
+    is_object_owner_permission = IsObjectOwner()
+    check_permission = is_object_owner_permission.has_object_permission(
+        request=mock_request,
+        view=mock_view,
+        obj=mock_obj
     )
 
     assert check_permission is False
