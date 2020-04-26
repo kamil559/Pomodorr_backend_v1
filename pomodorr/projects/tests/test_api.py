70e39284c3b433e156b58bd23b02364154c35e17
Kamil <kamil.daniewski@gmail.com>
2020-04-26 15:49:21 +0200

    Refactored ProjectsViewSet tests.

--- pomodorr/projects/tests/test_api.py
+++ pomodorr/projects/tests/test_api.py
@@ -41,8 +41,8 @@ class TestPriorityViewSet:
             ('color', factory.Faker('pystr', max_chars=19).generate()),
         ]
     )
-    def test_creates_priority_with_invalid_data(self, invalid_field_key, invalid_field_value, priority_data,
-                                                active_user, request_factory):
+    def test_create_priority_with_invalid_data(self, invalid_field_key, invalid_field_value, priority_data,
+                                               active_user, request_factory):
         priority_data[invalid_field_key] = invalid_field_value
         view = PriorityViewSet.as_view({'post': 'create'})
         request = request_factory.post(self.base_url, priority_data)
@@ -208,7 +208,7 @@ class TestPriorityViewSet:
 
 
 class TestProjectsViewSet:
-    def test_user_creates_new_project_with_valid_data(self, project_data, active_user, request_factory):
+    def test_create_project_with_valid_data(self, project_data, active_user, request_factory):
         url = reverse('api:project-list')
         view = ProjectsViewSet.as_view({'post': 'create'})
         request = request_factory.post(url, project_data)
@@ -232,8 +232,8 @@ class TestProjectsViewSet:
             ('user_defined_ordering', '')
         ]
     )
-    def test_user_tries_to_create_new_project_with_invalid_data(self, invalid_field_key, invalid_field_value,
-                                                                project_data, active_user, request_factory):
+    def test_create_project_with_invalid_data(self, invalid_field_key, invalid_field_value, project_data, active_user,
+                                              request_factory):
         project_data[invalid_field_key] = invalid_field_value
         url = reverse('api:project-list')
         view = ProjectsViewSet.as_view({'post': 'create'})
@@ -245,17 +245,10 @@ class TestProjectsViewSet:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert invalid_field_key in response.data
 
-    def test_user_tries_to_create_new_project_without_authorization(self, project_data, request_factory):
-        project_data = project_data.fromkeys(project_data, '')
-        url = reverse('api:project-list')
-        view = ProjectsViewSet.as_view({'post': 'create'})
-        request = request_factory.post(url, project_data)
+    def test_create_project_with_duplicated_name(self):
+        pass
 
-        response = view(request)
-
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    def test_user_gets_his_projects(self, project_create_batch, request_factory, active_user):
+    def test_get_project_list(self, project_create_batch, request_factory, active_user):
         url = reverse('api:project-list')
         view = ProjectsViewSet.as_view({'get': 'list'})
         request = request_factory.get(url)
@@ -267,50 +260,11 @@ class TestProjectsViewSet:
         assert response.data['count'] == 5
         assert response.data['results'] is not None
 
-    def test_user_tries_to_get_his_projects_without_authorization(self, project_create_batch, request_factory):
-        url = reverse('api:project-list')
-        view = ProjectsViewSet.as_view({'get': 'list'})
-        request = request_factory.get(url)
-
-        response = view(request)
-
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    def test_user_gets_project_detail(self, project_instance, request_factory, active_user):
-        url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
-        view = ProjectsViewSet.as_view({'get': 'retrieve'})
-        request = request_factory.get(url)
-        force_authenticate(request=request, user=active_user)
-
-        response = view(request, pk=project_instance.pk)
-
-        assert response.status_code == status.HTTP_200_OK
-        assert response.data is not None
-
-    def test_user_tries_to_get_project_detail_without_authorization(self, project_instance, request_factory):
-        url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
-        view = ProjectsViewSet.as_view({'get': 'retrieve'})
-        request = request_factory.get(url)
-
-        response = view(request, pk=project_instance.pk)
-
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    def test_user_tries_to_get_someone_elses_project_detail(self, active_user, project_instance_for_random_user,
-                                                            request_factory):
-        url = reverse('api:project-detail', kwargs={'pk': project_instance_for_random_user.pk})
-        view = ProjectsViewSet.as_view({'get': 'retrieve'})
-        request = request_factory.get(url)
-        force_authenticate(request=request, user=active_user)
-
-        response = view(request, pk=project_instance_for_random_user.pk)
-
-        assert response.status_code == status.HTTP_404_NOT_FOUND
-
     @pytest.mark.parametrize(
         'ordering',
         ['created_at', '-created_at', 'priority', '-priority', 'user_defined_ordering', '-user_defined_ordering'])
-    def test_user_gets_his_projects_ordered(self, ordering, project_create_batch, request_factory, active_user):
+    def test_get_project_list_ordered_by_valid_fields(self, ordering, project_create_batch, request_factory,
+                                                      active_user):
         url = reverse_query_params('api:project-list', query_kwargs={'ordering': ordering})
         view = ProjectsViewSet.as_view({'get': 'list'})
         request = request_factory.get(url)
@@ -319,8 +273,7 @@ class TestProjectsViewSet:
         response = view(request)
 
         assert response.status_code == status.HTTP_200_OK
-        assert response.data is not None
-        assert 'results' in response.data
+        assert response.data['results'] is not None
 
         response_result_ids = [record['id'] for record in response.data['results']]
         sorted_orm_fetched_projects = list(map(
@@ -330,9 +283,8 @@ class TestProjectsViewSet:
         assert response_result_ids == sorted_orm_fetched_projects
 
     @pytest.mark.parametrize('ordering', ['id', '-id', 'xyz', '-xyz'])
-    def test_user_gets_his_projects_ordered_by_forbidden_fields(self, ordering, project_create_batch,
-                                                                request_factory,
-                                                                active_user):
+    def test_get_project_list_ordered_by_invalid_fields(self, ordering, project_create_batch, request_factory,
+                                                        active_user):
         #  Ordering by non-existing fields for model or by fields not specified in serializer ordering
         #  should return records ordered by default ordering specified in serializer or model
 
@@ -343,8 +295,7 @@ class TestProjectsViewSet:
         response = view(request)
 
         assert response.status_code == status.HTTP_200_OK
-        assert response.data is not None
-        assert 'results' in response.data
+        assert response.data['results'] is not None
 
         response_result_ids = [record['id'] for record in response.data['results']]
         default_sorted_orm_fetched_projects = list(map(
@@ -352,8 +303,28 @@ class TestProjectsViewSet:
             ProjectSelector.get_active_projects_for_user(user=active_user).values_list('id', flat=True)))
         assert response_result_ids == default_sorted_orm_fetched_projects
 
-    def test_user_updates_his_project_with_valid_data(self, project_data, project_instance, active_user,
-                                                      request_factory):
+    def test_get_project_detail(self, project_instance, request_factory, active_user):
+        url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
+        view = ProjectsViewSet.as_view({'get': 'retrieve'})
+        request = request_factory.get(url)
+        force_authenticate(request=request, user=active_user)
+
+        response = view(request, pk=project_instance.pk)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data is not None
+
+    def test_get_someone_elses_project_detail(self, active_user, project_instance_for_random_user, request_factory):
+        url = reverse('api:project-detail', kwargs={'pk': project_instance_for_random_user.pk})
+        view = ProjectsViewSet.as_view({'get': 'retrieve'})
+        request = request_factory.get(url)
+        force_authenticate(request=request, user=active_user)
+
+        response = view(request, pk=project_instance_for_random_user.pk)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_update_project_with_valid_data(self, project_data, project_instance, active_user, request_factory):
         project_data['id'] = project_instance.id
         url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
         view = ProjectsViewSet.as_view({'put': 'update'})
@@ -377,8 +348,8 @@ class TestProjectsViewSet:
             ('user_defined_ordering', '')
         ]
     )
-    def test_user_updates_his_project_with_invalid_data(self, invalid_field_key, invalid_field_value, project_data,
-                                                        project_instance, active_user, request_factory):
+    def test_update_project_with_invalid_data(self, invalid_field_key, invalid_field_value, project_data,
+                                              project_instance, active_user, request_factory):
         project_data[invalid_field_key] = invalid_field_value
         url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
         view = ProjectsViewSet.as_view({'put': 'update'})
@@ -389,21 +360,8 @@ class TestProjectsViewSet:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert invalid_field_key in response.data
 
-    def test_user_updates_his_project_without_authorization(self, project_data, project_instance, active_user,
-                                                            request_factory):
-        project_data['id'] = project_instance.id
-        url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
-        view = ProjectsViewSet.as_view({'put': 'update'})
-
-        request = request_factory.put(url, project_data)
-
-        response = view(request, pk=project_instance.pk)
-
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    def test_user_tries_to_update_someone_elses_project(self, project_data, active_user,
-                                                        project_instance_for_random_user,
-                                                        request_factory):
+    def test_update_someone_elses_project(self, project_data, active_user, project_instance_for_random_user,
+                                          request_factory):
         project_data['id'] = project_instance_for_random_user.id
         url = reverse('api:project-detail', kwargs={'pk': project_instance_for_random_user.pk})
         view = ProjectsViewSet.as_view({'put': 'update'})
@@ -414,7 +372,7 @@ class TestProjectsViewSet:
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_user_deletes_his_project(self, project_instance, active_user, request_factory):
+    def test_delete_project(self, project_instance, active_user, request_factory):
         url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
         view = ProjectsViewSet.as_view({'delete': 'destroy'})
         request = request_factory.delete(url)
@@ -424,17 +382,7 @@ class TestProjectsViewSet:
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
-    def test_user_tries_to_delete_his_project_without_authorization(self, project_instance, request_factory):
-        url = reverse('api:project-detail', kwargs={'pk': project_instance.pk})
-        view = ProjectsViewSet.as_view({'delete': 'destroy'})
-        request = request_factory.delete(url)
-
-        response = view(request, pk=project_instance.pk)
-
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    def test_user_tries_to_delete_someone_elses_project(self, active_user, project_instance_for_random_user,
-                                                        request_factory):
+    def test_delete_someone_elses_project(self, active_user, project_instance_for_random_user, request_factory):
         url = reverse('api:project-detail', kwargs={'pk': project_instance_for_random_user.pk})
         view = ProjectsViewSet.as_view({'delete': 'destroy'})
         request = request_factory.delete(url)
