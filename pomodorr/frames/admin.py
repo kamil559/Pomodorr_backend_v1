9bc06482b9ebfa4e23f0b5d89e7fc10f24430c18
Kamil <kamil.daniewski@gmail.com>
2020-05-05 22:54:01 +0200

    Added admin site for DateFrame

--- pomodorr/frames/admin.py
+++ pomodorr/frames/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from pomodorr.frames.models import DateFrame
+from pomodorr.frames.selectors import DateFrameSelector
+
+
+class IsFinishedFilter(admin.SimpleListFilter):
+    title = 'is_finished status'
+    parameter_name = 'is_finished'
+
+    lookup_choices = (
+        ('1', _('Yes')),
+        ('0', _('No'))
+    )
+
+    def lookups(self, request, model_admin):
+        return self.lookup_choices
+
+    def queryset(self, request, queryset):
+        value = self.value()
+
+        if value == self.lookup_choices[0][0]:
+            return queryset.filter(is_finished=True)
+        elif value == self.lookup_choices[1][0]:
+            return queryset.filter(is_finished=False)
+        return queryset
 
 
 @admin.register(DateFrame)
 class DateFrameAdmin(admin.ModelAdmin):
-    pass
+    selector_class = DateFrameSelector(model_class=DateFrame)
+    list_display = ('__str__', 'frame_type', 'start', 'end', 'duration', 'task', 'is_finished')
+    fieldsets = [
+        (None, {'fields': ['start', 'end', 'frame_type', 'task']}),
+    ]
+    search_fields = ('frame_type', 'task__name', 'start', 'end')
+    list_filter = ('frame_type', IsFinishedFilter)
+
+    def get_queryset(self, request):
+        return self.selector_class.get_all_date_frames()
+
+    def is_finished(self, instance):
+        return instance.is_finished
+
+    is_finished.boolean = True
+    is_finished.admin_order_field = 'is_finished'
