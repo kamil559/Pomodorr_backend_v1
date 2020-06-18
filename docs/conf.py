489b8a22075a134bed706ce6b9c7a703e417db2d
Kamil <kamil.daniewski@gmail.com>
2020-06-18 15:25:24 +0200

    Removed the broken ReadTheDocs theme extension.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ author = "Kamil Daniewski"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
     'sphinx.ext.autodoc'
 ]
 
@@ -49,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
