6ab65eeec58e89007c4bb1aad74e09c56bf6c2f1
Kamil <kamil.daniewski@gmail.com>
2020-06-19 20:46:44 +0200

    Fixed domain name for the purpose of deployment tests.

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["pomodorr.io"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["pomodorr.tk"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -112,7 +112,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Pomodorr <noreply@pomodorr.io>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Pomodorr <noreply@pomodorr.tk>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
