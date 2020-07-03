d47a403f355e771fab2ef1538e2980ac71c560f5
Kamil <kamil.daniewski@gmail.com>
2020-07-03 19:03:42 +0200

    Removed misleading django settings module env.setdefault and replaced it with more intuitive ones. From now on, only running the django app with built in manage.py runserver command won't need configuring the environment variable. In case of using wsgi or asgi standalone servers the user will be obliged to specify django settings module in the .django envs file.

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,12 +1,9 @@
-import os
 from datetime import timedelta
 
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
 from celery.schedules import crontab
 from kombu import Queue
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery("pomodorr")
 
